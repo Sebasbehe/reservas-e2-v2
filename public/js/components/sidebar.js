c7530633733f28b2c38077eee231f7f4515bb940
Sebasbehe <sebastianpersmor2006@gmail.com>
2026-06-05 09:43:42 -0500

    m

--- public/js/components/sidebar.js
+++ public/js/components/sidebar.js
@@ -145,18 +145,3 @@ document.getElementById(
     }
     
 );
-const menuToggle =
-    document.getElementById("menuToggle");
-
-const sidebar =
-    document.querySelector(".sidebar");
-
-if (menuToggle && sidebar) {
-
-    menuToggle.addEventListener("click", () => {
-
-        sidebar.classList.toggle("collapsed");
-
-    });
-
-}
